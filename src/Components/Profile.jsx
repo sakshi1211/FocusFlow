dbd10bc2d4b74586f418512d3b1ffd53080f5d15
sakshi1211 <sakshimohitee6@gmail.comgit config --global user.email sakshimohitee6.in>
2025-11-09 16:09:08 +0530

    Create Profile.css, some changes in Profile.jsx & App.jsx, add profile image

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import ProfileImg from "../assets/Profile-img.png"
+import "./Profile.css";
+
 
 const Profile = () => {
 
+    const [userName, setUserName] = useState("");
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const storedName = JSON.parse(localStorage.getItem("userInfo"));
+        if (storedName && storedName.userName) {
+            setUserName(storedName.userName);
+        }
+    }, [])
 
     const handleLogout = () => {
         localStorage.removeItem("userInfo");
@@ -13,14 +24,31 @@ const Profile = () => {
 
     return (
 
-        <div>
+        <div className="profile-container relative items-center p-6 bg-[--color-bg-light] dark:bg-[--color-bg-dark] text-[--color-text-light] dark:text-[--color-text-dark] transition-colors duration-300">
+
+            <div>
+                <div className="profile-info shadow-lg p-6 rounded-2xl text-center">
+
+                    <img
+                        src={ProfileImg}
+                        alt="Profile"
+                        className="profile-img w-44 h-44 rounded-full mx-auto mb-3"
+                    />
+                    <h2 className="text-xl font-bold">{userName}</h2>
+
+                </div>
+
+            </div>
+
             <button
                 onClick={handleLogout}
-                className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600"
+                className="logout-btn absolute bottom-2 right-2 font-bold text-red-400 px-4 py-2 rounded-lg"
             >
                 Logout
             </button>
-        </div>
+
+        </div >
+
 
     )
 }
